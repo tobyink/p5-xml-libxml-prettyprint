bb27348ba1c77494c2123c7f31ff8c854851acb5
Toby Inkster <mail@tobyinkster.co.uk>
2014-09-07 10:01:47 +0100

    bump version; changelog
    --HG--
    branch : XML-LibXML-PrettyPrint

--- lib/XML/LibXML/PrettyPrint.pm
+++ lib/XML/LibXML/PrettyPrint.pm
@@ -15,7 +15,7 @@ use constant { EL_BLOCK => 1, EL_COMPACT => 2, EL_INLINE => 3};
 BEGIN
 {
 	$XML::LibXML::PrettyPrint::AUTHORITY = 'cpan:TOBYINK';
-	$XML::LibXML::PrettyPrint::VERSION   = '0.004';
+	$XML::LibXML::PrettyPrint::VERSION   = '0.005';
 }
 
 use Carp 0 qw(croak carp);
@@ -659,7 +659,7 @@ Toby Inkster E<lt>tobyink@cpan.orgE<gt>.
 
 =head1 COPYRIGHT AND LICENCE
 
-This software is copyright (c) 2011-2013 by Toby Inkster.
+This software is copyright (c) 2011-2014 by Toby Inkster.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
